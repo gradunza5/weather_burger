9cb16eb8b99bda49838f819d38fd2e390f5ed61b
Ben Reeves <ben.reeves@safetyio.com>
2024-04-01 14:19:05 -0400

    Initial commit. Template from Very Good CLI

---
+++ android/app/src/main/kotlin/com/havecoffeewillcode/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.havecoffeewillcode.weather_burger
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_staging.dart
@@ -0,0 +1,6 @@
+import 'package:weather_burger/app/app.dart';
+import 'package:weather_burger/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
